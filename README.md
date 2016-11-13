5839bb900cef774dfba474c82bdecf62ed77bc40
juju2143 <juju2143@gmail.com>
2016-11-13 18:04:56 -0500

    First commit with a WIP of Undertale

---
+++ README.md
@@ -0,0 +1,50 @@
+# TAS
+
+This repository contains Python scripts and a library I wrote designed to act as tool-assisted speedrun (TAS) movie files for PC games, that is, they automatically open a particular PC game, wait for the window to show up and send keypresses in order to complete them as fast as possible.
+
+Of course, since it only sends keypresses and does not ensure the state is the same each time the movie is replayed (unlike Hourglass), the movies produced by this software are unlikely to be accepted on TASVideos.org, and are also extremely prone to desyncs.
+Also, there is currently no way to record gameplay (although it should be possible). There is also no way to use savestates and the like.
+So you have to edit the movie file manually and replay it from start each time. Quite cumbersome, I know, but it works. The concept of "rerecords" might not be applicable here, either.
+For a real, state-of-the-art TAS, you might want to use Hourglass instead, although my scripts can be useful if you want to make a TAS of a simple game Hourglass does not support yet.
+Likewise, you can't submit these runs on any sort of speedrun community as they usually won't allow TASing and speedruns done with scripts and will in no way be considered world records there.
+I am not aware of any community that accepts TAS done with scripts such as this, but that would be nice.
+
+I initially started this project in AppleScript, it's a pretty powerful language for automation, but the key up/key down functions were lacking in functionality, so I ported it in Python with Quartz.CoreGraphics, which is much more powerful.
+There's still a bit of AppleScript left in there so it waits until the window shows up before starting the movie.
+
+## Library
+
+I'm aware there's a ton of libraries and software out there to automate keypresses and mouse actions, but this one is specifically designed for TASing, with the movie files supposed to look like, well, movie files, with all the keypresses and timing.
+This only works on macOS, but support of other OSes such as Windows and Linux can be added later.
+
+It contains a few useful functions pertaining to keypresses, mouse actions, opening software and waiting until the window show up, enough to make a good TAS.
+More functions will be added as they will be deemed useful.
+
+The library is found in `taslib.py`, you will find the documentation in the [wiki](https://github.com/juju2143/tas/wiki).
+You will need [PyObjC](https://pypi.python.org/pypi/pyobjc-framework-Quartz) (usually installed by default on macOS) and [py-applescript](https://pypi.python.org/pypi/py-applescript) installed.
+
+## Movie files
+
+A good movie file should contain at least this header
+
+```
+#!/usr/bin/env python
+from taslib import *
+```
+
+followed by a few comments (starting with `#`) with file information such as the name of the run, duration and information required in order to start the movie
+(ex.: `# Game Name (version) "category, if applicable" by username in 13:37.42`) 
+then a call to `openAndWait()` and the inputs. You can then run it with your python interpreter. You will find these scripts useful as examples.
+
+### In progress
+
+* `undertale.py` Undertale (v1.001) "hard mode" by juju2143
+
+## Timing
+
+Sure, you can derive the movie duration from the delay parameters, or measure the time with Python time functions, but I found an external timer such as [Llanfair](http://jenmaarai.com/llanfair/) particularly useful, especially if you're streaming on Twitch.
+You can then assign F5 to start/split and F6 to reset in the config, then use `kPress("f5")` and `kPress("f6")` in your movie to automatically control the timer in your script, without any signifiant adverse effect to the timing.
+
+## Links
+
+[Forum thread](https://codewalr.us/index.php?topic=1676.0)
---
+++ taslib.py
@@ -0,0 +1,205 @@
+from time import sleep
+from Quartz.CoreGraphics import *
+from subprocess import call, Popen, PIPE
+import applescript
+
+debug = False
+
+def debugMode(d):
+	debug = d
+
+def dprint(str):
+	if debug:
+		print(str)
+
+def keyCodeFromString(keyString):
+	keys = {
+		"a": 0,
+		"s": 1,
+		"d": 2,
+		"f": 3,
+		"h": 4,
+		"g": 5,
+		"z": 6,
+		"x": 7,
+		"c": 8,
+		"v": 9,
+		"b": 11,
+		"q": 12,
+		"w": 13,
+		"e": 14,
+		"r": 15,
+		"y": 16,
+		"t": 17,
+		"1": 18,
+		"2": 19,
+		"3": 20,
+		"4": 21,
+		"6": 22,
+		"5": 23,
+		"=": 24,
+		"9": 25,
+		"7": 26,
+		"-": 27,
+		"8": 28,
+		"0": 29,
+		"]": 30,
+		"o": 31,
+		"u": 32,
+		"[": 33,
+		"i": 34,
+		"p": 35,
+		"return": 36,
+		"l": 37,
+		"j": 38,
+		"'": 39,
+		"k": 40,
+		";": 41,
+		"\\": 42,
+		",": 43,
+		"/": 44,
+		"n": 45,
+		"m": 46,
+		".": 47,
+		"tab": 48,
+		" ": 49,
+		"`": 50,
+		"delete": 51,
+		"enter": 52,
+		"escape": 53,
+		"np.": 65,
+		"np*": 67,
+		"np+": 69,
+		"clear": 71,
+		"np/": 75,
+		"npenter": 76,
+		"np-": 78,
+		"np=": 81,
+		"np0": 82,
+		"np1": 83,
+		"np2": 84,
+		"np3": 85,
+		"np4": 86,
+		"np5": 87,
+		"np6": 88,
+		"np7": 89,
+		"np8": 91,
+		"np9": 92,
+		"f5": 96,
+		"f6": 97,
+		"f7": 98,
+		"f3": 99,
+		"f8": 100,
+		"f9": 101,
+		"f11": 103,
+		"f13": 105,
+		"f14": 107,
+		"f10": 109,
+		"f12": 111,
+		"f15": 113,
+		"help": 114,
+		"home": 115,
+		"pgup": 116,
+		"delete": 117,
+		"f4": 118,
+		"end": 119,
+		"f2": 120,
+		"pgdn": 121,
+		"f1": 122,
+		"left": 123,
+		"right": 124,
+		"down": 125,
+		"up": 126,
+	}
+	return keys[keyString]
+
+def openAndWait(app, process, window):
+	call(["open",app])
+	dprint(process + " opened, waiting for window...")
+	scpt = '''
+	on run {proc, win}
+		tell application "System Events"
+			repeat until window win of process proc exists
+			end repeat
+			set frontmost of process proc to true
+		end tell
+	end run
+	'''
+	#p = Popen(['osascript', '-', process, window], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+	#stdout, stderr = p.communicate(scpt)
+	applescript.AppleScript(scpt).run(process, window)
+	dprint("done.")
+def mEvent(type, posx, posy):
+	theEvent = CGEventCreateMouseEvent(None, type, (posx,posy), kCGMouseButtonLeft)
+	CGEventPost(kCGHIDEventTap, theEvent)
+#	CFRelease(theEvent)
+def mMove(posx, posy, delay=0):
+	mEvent(kCGEventMouseMoved, posx, posy)
+	dprint("mouse moved")
+	sleep(delay)
+def mDown(posx, posy, delay=0):
+	mEvent(kCGEventLeftMouseDown, posx, posy)
+	dprint("mouse down")
+	sleep(delay)
+def mUp(posx, posy, delay=0):
+	mEvent(kCGEventLeftMouseUp, posx, posy)
+	dprint("mouse up")
+	sleep(delay)
+def mClick(posx, posy, delay=0, reps=1):
+	for i in range(0,reps):
+		mDown(posx, posy)
+		mUp(posx, posy)
+		sleep(delay)
+def mHold(posx, posy, delay=0, reps=1):
+	for i in range(0,reps):
+		mDown(posx, posy)
+		sleep(delay)
+		mUp(posx, posy)
+def mCurPos():
+	theEvent = CGEventCreate(None)
+	return CGEventGetLocation(theEvent)
+def mClickHere(delay=0, reps=1):
+	posx, posy = mCurPos()
+	mClick(posx, posy, delay, reps)
+def mDownHere(delay=0):
+	posx, posy = mCurPos()
+	mDown(posx, posy, delay)
+def mUpHere(delay=0):
+	posx, posy = mCurPos()
+	mClick(posx, posy, delay)
+def kEvent(key, isDown):
+	theEvent = CGEventCreateKeyboardEvent(None, key, isDown)
+	CGEventPost(kCGHIDEventTap, theEvent)
+#	CFRelease(theEvent)
+def kDown(key, delay=0):
+	code = keyCodeFromString(key)
+	kEvent(code,True)
+	dprint("key down")
+	sleep(delay)
+def kUp(key, delay=0):
+	code = keyCodeFromString(key)
+	kEvent(code,False)
+	dprint("key up")
+	sleep(delay)
+def kPress(key, delay=0, reps=1):
+	for i in range(0,reps):
+		kDown(key)
+		kUp(key)
+		sleep(delay)
+def kHold(key, delay=0, reps=1):
+	for i in range(0,reps):
+		kDown(key)
+		sleep(delay)
+		kUp(key)
+def kType(keys, delay=0, delay2=0, reps=1):
+	for i in range(0,reps):
+		for c in keys:
+			kPress(c, delay)
+		sleep(delay2)
+def kHolds(keys, delay=0, reps=1):
+	for i in range(0,reps):
+		for c in keys:
+			kDown(key)
+		sleep(delay)
+		for c in keys:
+			kUp(key)
